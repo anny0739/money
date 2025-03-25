4f6c590c0aa75e299900bf7f535bc06be75bba4e
areum.k <areum.k@woowahan.com>
2025-03-25 19:37:46 +0900

    main-1 rebase test

--- run-local.sh
+++ run-local.sh
@@ -3,3 +3,4 @@
 current_dir=$(cd "$(dirname "$0")"; pwd)
 db_container_name=mysqllocal
 docker build --tag=$db_container_name $current_dir/docker/ && docker run -t -p=3306:3306 --name=db_container_name $db_container_name:latest
+
